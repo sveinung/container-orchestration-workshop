253bd46bc50762d19109af1ecce67a4b17041963
Stian Mathiassen <stian.mathiassen@bekk.no>
2016-10-24 17:44:43 +0200

    Lagre resultater i database. Bruker en in-memory-database.
    Kan overstyres med JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD env variabler

--- backend/src/main/kotlin/no/bekk/backend/BackendApplication.kt
+++ backend/src/main/kotlin/no/bekk/backend/BackendApplication.kt
@@ -1,9 +1,13 @@
 package no.bekk.backend
 
+import com.zaxxer.hikari.HikariDataSource
+import liquibase.integration.spring.SpringLiquibase
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.context.annotation.Bean
+import javax.sql.DataSource
 
 @SpringBootApplication
 open class BackendApplication {
@@ -14,6 +18,27 @@ open class BackendApplication {
         registration.addUrlPatterns("*")
         registration.setName("corsFilter")
         return registration
+    }
+
+    @Bean
+    open fun datasource(): DataSource {
+        val pool = HikariDataSource()
+
+        pool.jdbcUrl = System.getenv("JDBC_URL") ?: "jdbc:h2:mem:test"
+        pool.username = System.getenv("JDBC_USERNAME") ?: "user"
+        pool.password = System.getenv("JDBC_PASSWORD") ?: "pass"
+        return pool
+    }
+
+    @Bean
+    @Autowired
+    open fun liquibase(ds: DataSource): SpringLiquibase {
+        val liquibase = SpringLiquibase();
+
+        liquibase.dataSource = ds
+        liquibase.changeLog = "classpath:db-changelog.xml"
+
+        return liquibase
     }
 }
 
--- backend/src/main/kotlin/no/bekk/backend/InMemoryResource.kt
+++ backend/src/main/kotlin/no/bekk/backend/InMemoryResource.kt
@@ -12,12 +12,12 @@ class InMemoryResource {
     val todos: MutableMap<Int, Todo> = HashMap()
     val counter: AtomicInteger = AtomicInteger(1)
 
-    @RequestMapping(value = "/v1", method = arrayOf(RequestMethod.GET))
+    @RequestMapping(value = "/v0", method = arrayOf(RequestMethod.GET))
     fun showAll() : List<Todo> {
         return ArrayList(todos.values)
     }
 
-    @RequestMapping(value = "v1", method = arrayOf(RequestMethod.POST))
+    @RequestMapping(value = "v0", method = arrayOf(RequestMethod.POST))
     fun new(@RequestBody todo: Todo) : ResponseEntity<Any> {
         if (todo.id != -1) {
             return ResponseEntity(HttpStatus.BAD_REQUEST);
@@ -29,7 +29,7 @@ class InMemoryResource {
         return ResponseEntity(todoWithId, HttpStatus.OK)
     }
 
-    @RequestMapping(value = "v1", method = arrayOf(RequestMethod.PUT))
+    @RequestMapping(value = "v0", method = arrayOf(RequestMethod.PUT))
     fun update(@RequestBody todo: Todo) : ResponseEntity<Any> {
         if (!todos.contains(todo.id)) {
             return ResponseEntity(HttpStatus.BAD_REQUEST)
@@ -38,7 +38,7 @@ class InMemoryResource {
         return ResponseEntity(todo, HttpStatus.OK)
     }
 
-    @RequestMapping(value = "v1/{id}", method = arrayOf(RequestMethod.DELETE))
+    @RequestMapping(value = "v0/{id}", method = arrayOf(RequestMethod.DELETE))
     fun delete(@PathVariable("id") id: Int) : ResponseEntity<Any> {
         if (!todos.contains(id)) {
             return ResponseEntity(HttpStatus.BAD_REQUEST)
